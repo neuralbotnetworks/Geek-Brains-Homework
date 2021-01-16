d362b09817c45e96b87a10b5f493b20177a323be
Алексей <neuralbot@protonmail.com>
2021-01-16 17:06:38 +0300

    Completed the task 5 of 3 lesson

--- Python/Lesson3/task5.py
+++ Python/Lesson3/task5.py
@@ -18,3 +18,25 @@
 #  то вначале нужно добавить сумму этих чисел
 #  к полученной ранее сумме и
 #  после этого завершить программу.
+
+def calc(*nums):
+    sum = 0
+    flag = False
+    for num in nums:
+        try:
+            if num:
+                sum += int(num)
+        except ValueError:
+            flag = True
+    return sum, flag
+
+calc_result = 0
+
+while True:
+    user_nums = input('Введите числа через пробел: ').split(' ')
+    sum, stop_flag = calc(*user_nums)
+    calc_result += sum
+    print(f'Cумма чисел: {calc_result}')
+
+    if stop_flag:
+        break
