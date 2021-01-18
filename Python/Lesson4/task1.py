9029a8e5c1934696408cd6a94bdf3b59e7f07799
Алексей <neuralbot@protonmail.com>
2021-01-18 16:15:11 +0300

    completed task

--- Python/Lesson4/task1.py
+++ Python/Lesson4/task1.py
@@ -4,15 +4,15 @@
 #  Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.
 from sys import argv
 
-task1, working_hours, rate_per_hour, bonus = argv
+_, working_hours, rate_per_hour, bonus = argv
 
-print("Имя скрипта: ", task1)
+
 print("Количество отработаных часов: ", working_hours)
 print("Плата за 1 час работы: ", rate_per_hour)
 print("Премия: ", bonus)
 
-working_hours = int(working_hours)
-rate_per_hour = int(rate_per_hour)
-bonus = int(bonus)
+working_hours = float(working_hours)
+rate_per_hour = float(rate_per_hour)
+bonus = float(bonus)
 
 print((working_hours * rate_per_hour) + bonus)
