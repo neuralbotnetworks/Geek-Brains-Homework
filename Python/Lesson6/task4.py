9636c38ea459ee43edcb485911876b767f4722c7
Алексей <neuralbot@protonmail.com>
2021-01-25 19:16:45 +0300

    fix lesson 6 tasks

--- Python/Lesson6/task4.py
+++ Python/Lesson6/task4.py
@@ -1,132 +1,95 @@
 # Реализуйте базовый класс Car.
-
-#  У данного класса должны быть следующие 
-
-# атрибуты:
-#  speed,
-#  color,
-#  name,
-#  is_police (булево). 
+#  У данного класса должны быть следующие атрибуты:
+#  + speed,
+#  + color,
+#  + name,
+#  + is_police (булево). 
 # 
-#  А также 
-# 
-# методы: 
-#  go, 
-#  stop, 
-#  turn(direction)
+#  А также методы: 
+#  + go, 
+#  + stop, 
+#  + turn(direction)
 
-# которые должны сообщать, что машина 
-#  поехала, 
-#  остановилась, 
-#  повернула (куда).
+#  Kоторые должны сообщать, что машина 
+#  + поехала, 
+#  + остановилась, 
+#  + повернула (куда).
 
-# Опишите несколько 
-# 
-# дочерних классов: 
-#  TownCar, 
-#  SportCar,
-#  WorkCar,
-#  PoliceCar.
+# Опишите несколько дочерних классов: 
+#  + TownCar, 
+#  + SportCar,
+#  + WorkCar,
+#  + PoliceCar.
 
-# Добавьте 
-# в базовый класс 
-# метод 
-#  show_speed,
-# 
-#  который должен показывать текущую скорость автомобиля.
+# Добавьте в базовый класс метод 
+#  + show_speed,
+# который должен показывать текущую скорость автомобиля.
 
 # Для классов 
-# TownCar и 
-# WorkCar 
-
+# TownCar и WorkCar 
 # переопределите метод 
 # show_speed.
 
 # При значении скорости свыше 
-
-# 60 (TownCar) и 
-# 40 (WorkCar) 
-# 
+# 60 (TownCar) и 40 (WorkCar) 
 # должно выводиться сообщение о превышении скорости.
 
-# Создайте экземпляры классов, передайте значения атрибутов.
-# Выполните доступ к атрибутам, выведите результат. 
-# Выполните вызов методов и также покажите результат.
-
-
+# Создайте экземпляры классов, 
+# передайте значения атрибутов.
+# Выполните доступ к атрибутам, 
+# выведите результат. 
+# Выполните вызов методов и также 
+# покажите результат.
 
 class Car:
+    def __init__(self, name, color, speed, is_police):
+#        print('New construction action')
+        self.name = name
+        self.color = color
+        self.speed = speed
+        self.is_police = is_police
 
-    speed: float
-    color: str
-    name: str
-    is_police: bool
+    def car_go(self):
+        return f'{self.color} автомобиль {self.name} движется.'
 
-    def go(self):
-        return print('Движение')
+    def car_stop(self):
+        return f'{self.color} автомобиль {self.name} остановился.'
 
-    def stop(self):
-        return print('Остановка')
+    def car_turn(self, direction):
+        return f'{self.color} автомобиль {self.name} повернул {direction}'
 
-    def turn(self, direction):
-        return print(f'Поворот в {direction}')
-
-    def show_speed(self):
-        return self.speed
-
-
-class PoliceCar(Car):
-    is_police = True
-    name = 'GM'
-    color = 'Black'
-    speed = 150
-
-class TownCar(Car):
-    is_police = False
-    name = "VW"
-    color = "White"
-    speed = 90
-
-    def show_speed(self):
-        if self.speed > 60:
-            return 'Превышение скорости!! ' + str(self.speed)
-        return str(self.speed)
+    def car_show_speed(self):
+        return f'{self.color} автомобиль {self.name} движется со скоростью {self.speed} км/ч.'
 
 class WorkCar(Car):
-    is_police = False
-    name = 'ГАз NEXT'
-    color = 'Green'
-    speed = 60
-
-    def show_speed(self):
+    def car_show_speed(self):
         if self.speed > 40:
-            return 'Превышение скорости!! ' + str(self.speed)
-        return str(self.speed)
+            return f'{self.color} автомобиль {self.name} превысил скорость на {self.speed - 40} км/ч.'
+        return f'{self.color} автомобиль {self.name} движется со скоростью {self.speed} км/ч.'
+ 
+class TownCar(Car):
+    def car_show_speed(self):
+        if self.speed > 60:
+            return f'{self.color} автомобиль {self.name} превысил скорость на {self.speed - 60} км/ч.'
+        return f'{self.color} автомобиль {self.name} движется со скоростью {self.speed} км/ч.'
 
-class SportCar(Car):
-    is_police = False
-    name = 'Porshe 911 Targa 4S'
-    color = 'Gray'
-    speed = 180.0
 
+sportcar = Car('Porshe 911 Targa 4S', 'Серый', 220, False)
+policecar = Car('General Motors', 'Чёрный', 150, True)
+towncar = TownCar('Volkswagen Tiguan', 'Белый', 55, False)
+workcar = WorkCar('Газ Next', 'Зелёный', 70, False)
 
+print(towncar.__dict__)
+print(policecar.__dict__)
+print(workcar.__dict__)
+print(sportcar.__dict__)
 
-tc = TownCar()
-pc = PoliceCar()
-sc = SportCar()
-wc = WorkCar()
-
-print(tc.__dict__)
-print(pc.__dict__)
-print(wc.__dict__)
-print(sc.__dict__)
-
-pc.go()
-print(wc.turn(direction = 'не туда'))
-
-sc.speed = 150
-
-print(tc.__dict__)
-print(pc.__dict__)
-print(wc.__dict__)
-print(sc.__dict__)
+towncar.speed = 90
+sportcar.speed = 150
+print(policecar.car_go())
+print(policecar.car_turn(direction = 'не туда!'))
+print(workcar.car_turn(direction = 'вправо.'))
+workcar.speed = 20
+print(workcar.car_show_speed())
+print(towncar.car_show_speed())
+print(sportcar.car_show_speed())
